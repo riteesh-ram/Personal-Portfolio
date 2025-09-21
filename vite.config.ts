d9dafda2f87558d837a0f326d5a3a6fad868f01e
riteesh-ram <bollavaramgolla.riteeshramchander.18cse@bmu.edu.in>
2025-09-21 20:17:23 +0100

    Deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(() => ({
-  base: "/Personal-Portfolio/", // Your repository name
+  base: "/Personal-Portfolio/", // This is crucial!
   server: {
     host: "::",
     port: 8080,
