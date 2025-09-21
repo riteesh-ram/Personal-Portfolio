d27852020180a7e5073f0c238bf86969befd8058
riteesh-ram <bollavaramgolla.riteeshramchander.18cse@bmu.edu.in>
2025-09-21 19:47:11 +0100

    fixing build and deployment issues

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig(() => ({
+  base: "/Personal-Portfolio/", // Your repository name
   server: {
     host: "::",
     port: 8080,
